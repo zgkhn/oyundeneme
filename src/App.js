8bc023a070d90cfc43e2fd814cc053ef7e51bf51
zgkhn <gkhnznkldk@gmail.com>
2023-10-15 13:54:56 +0300

    bitti gibim

--- src/App.js
+++ src/App.js
@@ -1,81 +1,43 @@
-import React, { Component } from 'react';
-import './App.css'; // CSS dosyanızın adınıza göre düzenleyin
+import React from 'react';
+import './App.css';
+import Table from './Table';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      grid: Array(9)
-        .fill(null)
-        .map(() =>
-          Array(9)
-            .fill(null)
-            .map(() => ({ 
+function App() {
+  return (
+    <div className="App">
 
-              thickBorder: false,
+      <div className="grid-container">
+      <div className="grid-item"></div>
 
-      
+        <div className="grid-item item1"><h3>Kutu Oyunu (Dots and Boxes) Oyun Kuralı ve Oynanışı</h3>
+          <p>
+            Kutu Oyunu, iki oyunculu bir strateji oyunudur. Oyuncular, sırayla bir noktadan diğerine çizgi çekerek birer kare oluşturmaya çalışırlar. Oyunun amacı, kareleri tamamlamak ve en fazla kareyi kazanmaktır.
+          </p></div>
+        <div className="grid-item item2"><Table stn={29} str={29} p1={"red"} p2={"blue"} /></div>
+        <div className="grid-item item3">
+          <h3>Oyun Kuralları</h3>
+          <p>
+          * Oyuncular sırayla bir çizgi çizerler. Bir çizgi, iki noktayı birleştirir ve yalnızca dikey veya yatay olabilir. Çizgilerin köşegen olması veya iki noktanın zaten birleşik olduğu bir çizgi çizilmesi yasaktır.</p>
+          <p>* Bir oyuncu bir kare tamamladığında (dört çizgi ile çevrili), o kareyi kendisi için işaretler ve bir ekstra hamle hakkı kazanır.
+Oyuncu, bir kareyi tamamladığında sırası devam eder ve oyun sona erene kadar bu şekilde devam eder.</p>
+          
+          <p>* Oyun sona erdiğinde, tüm kareler tamamlanmış olmalıdır. </p>
+          <p>* Oyun sona erdiğinde Kazanan, daha fazla kareyi tamamlayan oyuncudur.
+ </p>
 
-
-            }))
-        ),
-    };
-  }
-
-  handleSquareClick = (row, col) => {
-    const updatedGrid = this.state.grid.map((r, rowIndex) =>
-      r.map((square, colIndex) => {
-        if (rowIndex === row && colIndex === col) {
-          return { thickBorder: !square.thickBorder };
-        }
-        return square;
-      })
-    );
-
-    this.setState({ grid: updatedGrid });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <table>
-          <tbody>
-            {this.state.grid.map((row, rowIndex) => (
-              <tr key={rowIndex}>
-                {row.map((square, colIndex) => (
-
-                  colIndex % 2 === 0 && rowIndex % 2 === 0 ? (<>
-                  <td
-                    className={square.thickBorder ? 'thick-border' : 'td-a'}
-                  ></td>
-                  
-                  </>)
-: colIndex % 2 !== 0 && rowIndex % 2 !== 0 ?
-(<>
-<td
-                    key={colIndex}
-                    className={'td-c'}
-                    onClick={() => this.handleSquareClick(rowIndex, colIndex)}
-                  >{rowIndex},{colIndex},</td>
-</>):(<>
-<td
-                    key={colIndex}
-                    className={square.thickBorder ? 'thick-border' : 'td-b'}
-                    onClick={() => this.handleSquareClick(rowIndex, colIndex)}
-                  ></td>
-</>)
-
-                ))}
+</div>
+          <div className="grid-item"></div>
 
 
-                
-              </tr>
-            ))}
-          </tbody>
-        </table>
       </div>
-    );
-  }
+
+
+      </div>
+
+
+
+
+      );
 }
 
-export default App;
+      export default App;
