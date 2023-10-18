a133ff66f4787ab934b5df7d8f3f8fc4f8c65f63
zgkhn <gknznkldk@gmail.com>
2023-10-18 23:01:53 +0300

    tfr

--- src/Table.jsx
+++ src/Table.jsx
@@ -11,20 +11,60 @@ import Sol from "./img/v.png"
 import Yatay from "./img/d.png"
 import Sag from "./img/c.png"
 import Dikey from "./img/y.png"
+import IconButton from '@mui/material/IconButton';
+import Fingerprint from '@mui/icons-material/Fingerprint';
 function Table({ stn, str, p1, p2 }) {
+  const [boxColor, setBoxColor] = useState("initial"); // Başlangıç rengi
 
+  const handleMouseEnter = () => {
+    setBoxColor("red"); // Rengi değiştirin
+  };
+
+  const handleMouseLeave = () => {
+    setBoxColor("initial"); // Başlangıç rengine geri döndürün
+  };
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [open, setOpen] = React.useState(false);
-  const [placement, setPlacement] = React.useState();
+  const [dikey, setDikey] = React.useState();
+  const [yatay, setYatay] = React.useState();
+  const [deneme, setdeneme] = React.useState();
 
-  const handleClick = (newPlacement) => (event) => {
+  const handleClick = (dikey,yatay) => (event) => {
     setAnchorEl(event.currentTarget);
-    setOpen((prev) => placement !== newPlacement || !prev);
-    setPlacement(newPlacement);
+    setOpen(true);
+    setDikey(dikey);
+    setYatay(yatay);
+
+
   };
 
+  const sec = (dikey,yatay) => (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+    setDikey(dikey);
+    setYatay(yatay);
 
+
+  };
+
+  const handlePopoverClose = () => {
+    setOpen(false);
+    setDikey("");
+    setYatay("");
+  };
+  const guncelle = (deger) => {
+
+    const updatedTableData = [...tableData];
+    updatedTableData[dikey][yatay].veri = deger;
+    setTableData(updatedTableData);
+
+
+    setOpen(false);
+    setDikey("");
+    setYatay("");
+  };
+console.log(deneme)
   
   const [tableData, setTableData] = useState([]);
   const [player, setPlayer] = useState(p1);
@@ -39,7 +79,7 @@ function Table({ stn, str, p1, p2 }) {
     for (let i = 0; i < str; i++) {
       const row = [];
       for (let j = 0; j < stn; j++) {
-        row.push({ cellText: `Cell ${i}-${j}`, isClicked: false, kazanan: "" });
+        row.push({ cellText: `Cell ${i}-${j}`, isClicked: false, veri: "",imgDikey : false,imgYatay : false,imgSag : false,imgSol : false });
       }
       table.push(row);
     }
@@ -118,15 +158,23 @@ function Table({ stn, str, p1, p2 }) {
       </div>
       <div style={{ color: player, height: "50px", fontSize: '20px' }}
       >
-<Popper open={open} anchorEl={anchorEl} placement={placement} transition>
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={350}>
-            <Paper>
-              <Typography sx={{ p: 2 }}>The content of the Popper.</Typography>
-            </Paper>
-          </Fade>
-        )}
-      </Popper>
+<Popper open={open} onClose={handlePopoverClose} anchorEl={anchorEl} placement="bottom" transition modifiers={[
+  {
+    name: 'arrow',
+    enabled: true,
+   
+  },
+]}>
+  {({ TransitionProps }) => (
+    <Fade {...TransitionProps} timeout={10}>
+      <Paper>
+        <Button onClick={() => guncelle("O")}>O</Button>
+        <Button onClick={() => guncelle("S")}>S</Button>
+      </Paper>
+    </Fade>
+  )}
+</Popper>
+
       </div>
       <div style={{ color: player, height: "50px", fontSize: '20px' }}
       >
@@ -141,16 +189,27 @@ function Table({ stn, str, p1, p2 }) {
               {row.map((cell, colIndex) => (
                 <td
                   className={"sos"}
-                  onClick={handleClick('bottom')}
+                  //onClick={handleClick(rowIndex,colIndex)}
+                  onClick={ cell.veri ? sec(rowIndex,colIndex): handleClick(rowIndex,colIndex)}
+
                                   >
-                  <img src={Dikey} className="resim" alt="Resim 1" />
+
+
+
+                  {cell.imgDikey ? (<><img src={Dikey} className="resim" alt="Resim 1" /></>) :
+                  cell.imgYatay ? ( <>                 <img src={Yatay} className="resim" alt="Resim 1" /></>                  ) :
+                  cell.imgSag ? (<><img src={Sag} className="resim" alt="Resim 1" /></>):
+                  cell.imgSol ? (<><img src={Sol} className="resim" alt="Resim 1" /></>):""}
+
+                  {/* <img src={Dikey} className="resim" alt="Resim 1" />
                   <img src={Yatay} className="resim" alt="Resim 1" />
                   <img src={Sol} className="resim" alt="Resim 1" />
-                  <img src={Sag} className="resim" alt="Resim 1" />
-                  {/* <span>{rowIndex.toString()+colIndex.toString()}</span> */}
+                  <img src={Sag} className="resim" alt="Resim 1" /> */}
 
 
 
+
+                  <span>{cell.veri}</span>
 
                   
                 </td>
@@ -160,6 +219,20 @@ function Table({ stn, str, p1, p2 }) {
           ))}
         </tbody>
       </table>
+      {/* {open && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            background: 'transparent',
+            zIndex: 999,
+          }}
+          onClick={handlePopoverClose}
+        ></div>
+      )} */}
     </div>
   );
 }
